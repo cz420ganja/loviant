a69e6faf1694ef38819fdbdf46909168287d3b6d
Conrad Campbell <loviantdev@protonmail.com>
2026-05-08 18:14:12 +0400

    Add tab icon and seamless companion grid

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,11 @@ import { AgeGate } from "../components/AgeGate";
 export const metadata = {
   title: "Loviant",
   description: "AI companion image and video studio",
+  icons: {
+    icon: "/images/loviant-tab-icon.png",
+    shortcut: "/images/loviant-tab-icon.png",
+    apple: "/images/loviant-tab-icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
